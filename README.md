49a9c9d3198a4488510d4a4f92057e05e25d8bab
Cthulhux <>
2022-04-29 14:18:42 +0000

    Done?
    FossilOrigin-Name: 3ca25fc725f2a653396a519037b033e7643eb75a7a4c6e352623ed8f3e3eb20f

---
+++ README.md
@@ -0,0 +1,99 @@
+# zb: a zip blog
+
+This is my approach at creating a self-contained website. The whole site will be embedded as a Zip archive right in the server binary file, so it is probably the most portable website you'll ever have.
+
+One of `zb`'s unique features it that it will render HTML, Markdown and Org-Mode files, so if you prefer to write `.org` files instead of HTML, `zb` has you covered.
+
+## Motivation
+
+I first saw the concept of *single binary websites* in [Ted Unangst's blog](https://flak.tedunangst.com/post/the-three-line-single-binary-compiler-free-blog) where he complained that Go couldn't do this just as easily, because of its broken support for embedded Zip files. As I play with both Go and Rust every now and then, I thought "how hard could it be to do it in Rust?".
+
+Quite surprisingly (for me), it wasn't really hard at all.
+
+## How do I use this?
+
+You'll need a Zip file, containing (at least) one of the following files in the root path:
+
+* `index.htm`
+* `index.html`
+* `index.md`
+* `index.org`
+
+If you prefer to use a different file name, you can pass `--defaultpage name.ext` when starting the application.
+
+Everything else in that Zip file can be any file of your choice. Relative references between them will work. (If they don't, please file a bug.)
+
+### First steps...
+
+* Fetch the latest code with Fossil ...
+  
+      > fossil clone https://code.rosaelefanten.org/zb
+      > cd zb
+  
+  ... or Git:
+  
+      > fossil clone https://github.com/dertuxmalwieder/zb
+      > cd zb
+
+* Install Rust, then build `zb` with `cargo`:
+  
+      > cargo build --release
+
+### On Unix and Unix-like systems
+
+Now, concatenate your `zb` binary with your Zip file. Assuming your Zip file is named `index.zip`:
+
+    % cat ./target/release/zb index.zip > ./zb
+
+### On Windows
+
+Now, concatenate your `zb.exe` with your Zip file. Assuming your Zip file is named `index.zip`:
+
+    # PowerShell
+    > cmd /c copy /b .\target\release\zb.exe+.\index.zip .\zb.exe
+    
+    # cmd
+    > copy /b .\target\release\zb.exe+.\index.zip .\zb.exe
+
+### Once done ...
+
+Run the concatenated `zb` binary and your website will be delivered from port 8000. (You can change that: `zb --port 8081` would make it run on port 8081 instead.) From now on, every time you want to update your website, just create a new Zip file and repeat the concatenation. (It should be easy to automatize that task.)
+
+#### Automatic converted HTML routing
+
+It is important to note that the file extensions for `.org`, `.md` and HTML files are optional, so the file `stuff/demofile.md` can be reached over `localhost:8000/stuff/demofile` as well.
+
+The search order for all files is:
+
+1. `/[path]`
+
+2. `/[path].md`
+
+3. `/[path].org`
+
+4. `/[path].htm`
+
+5. `/[path].html`
+
+## How to contribute code
+
+1. Read and agree to the [Code of ~~Conduct~~ Merit](CODE_OF_CONDUCT.md).
+2. Implicitly agree to the [LICENSE](LICENSE). Nobody reads those. I don't either.
+3. Find out if anyone has filed a GitHub Issue or even sent a Pull Request yet. Act accordingly.
+4. Send me a patch, either via e-mail (`git at tuxproject dot de`) or as a GitHub Pull Request. Note that GitHub only provides a mirror, so you'd double my work if you choose the latter. :-)
+
+If you do that well (and regularly) enough, I'll probably grant you commit access to the upstream Fossil repository.
+
+## Donations
+
+Writing this software and keeping it available is eating some of the time which most people would spend with their friends. Naturally, I absolutely accept financial compensation.
+
+* PayPal: [GebtmireuerGeld](https://paypal.me/gebtmireuergeld)
+* Liberapay: [Cthulhux](https://liberapay.com/Cthulhux/donate)
+
+Thank you.
+
+## Contact
+
+* Mastodon: [@tux0r](https://layer8.space/@tux0r)
+* Twitter: [@tux0r](https://twitter.com/tux0r)
---
+++ src/main.rs
@@ -0,0 +1,160 @@
+/*
+ * The contents of this file are subject to the terms of the
+ * Common Development and Distribution License, Version 1.1 only
+ * (the "License").  You may not use this file except in compliance
+ * with the License.
+ *
+ * See the file LICENSE in this distribution for details.
+ * A copy of the CDDL is also available via the Internet at
+ * https://spdx.org/licenses/CDDL-1.1.html
+ *
+ * When distributing Covered Code, include this CDDL HEADER in each
+ * file and include the contents of the LICENSE file from this
+ * distribution.
+ */
+
+use actix_web::{get, http::header, web, App, HttpResponse, HttpServer};
+use clap::Parser;
+use comrak::{markdown_to_html, ComrakOptions};
+use orgize::Org;
+use std::{env, ffi::OsStr, fs, io::Read, path::Path, process};
+use zip::ZipArchive;
+
+#[derive(Parser)]
+#[clap(version, about, long_about = None)]
+struct Args {
+    #[clap(long, help = "Set the port on which to run", default_value_t = 8000)]
+    port: u16,
+
+    #[clap(long, help = "Set the standard index page", default_value = "index")]
+    defaultpage: String,
+}
+
+async fn dispatch(filepath: &str) -> HttpResponse {
+    // Builds a response, based on the file's extension inside
+    // the ZIP file.
+    let args: Vec<_> = env::args().collect();
+
+    // This is where the magic happens:
+    let zipfile = fs::File::open(Path::new(&args[0])).unwrap();
+    let mut archive = ZipArchive::new(&zipfile).unwrap();
+
+    // Now the problem is that, if we call /index, it can be a file
+    // named "index.org" as well. Try to determine whether that
+    // exists:
+    let html_extensions = ["md", "org", "htm", "html"];
+    let potential_files = [
+        filepath.to_string(),
+        format!("{}.md", filepath),
+        format!("{}.org", filepath),
+        format!("{}.htm", filepath),
+        format!("{}.html", filepath),
+    ];
+
+    let extension: String;
+
+    for f in archive.clone().file_names() {
+        for potential_file in &potential_files {
+            if f == potential_file {
+                // This is the file we want.
+                // Get a handle on the file:
+                let mut file = archive.by_name(f).unwrap();
+
+                // Determine the file extension:
+                extension = String::from(
+                    Path::new(f)
+                        .extension()
+                        .and_then(OsStr::to_str)
+                        .ok_or("")
+                        .unwrap(),
+                );
+
+                if (&html_extensions).iter().any(|e| e == &extension) {
+                    // This is a HTML file.
+                    let mut contents = String::new();
+                    file.read_to_string(&mut contents);
+
+                    // - .md: Pass to the Markdown parser.
+                    // - .org: Pass to the org-mode parser.
+                    // - .htm(l): Just pass it to the client (as HTML).
+                    let html_output = match extension.as_str() {
+                        "md" => md_to_html(&contents),
+                        "org" => org_to_html(&contents),
+                        "htm" => contents.clone(),
+                        "html" => contents.clone(),
+                        &_ => "".to_string(), // This will not happen.
+                    };
+
+                    return HttpResponse::Ok()
+                        .insert_header(header::ContentType(mime::TEXT_HTML_UTF_8))
+                        .body(html_output);
+                } else {
+                    // If we reach this point, the requested file is not
+                    // a (supported) HTML file. Let's return a $ContentType
+                    // file instead and pass it to the client statically.
+                    let mut contents = Vec::new();
+                    file.read_to_end(&mut contents);
+                    return HttpResponse::Ok()
+                        .insert_header(header::ContentType(
+                            mime_guess::from_ext(&extension).first_or_text_plain(),
+                        ))
+                        .body(contents);
+                }
+            }
+        }
+    }
+
+    // Else, file not found.
+    HttpResponse::NotFound().finish()
+}
+
+fn md_to_html(in_str: &str) -> String {
+    // Converts the Markdown input string <in> to HTML.
+    markdown_to_html(in_str, &ComrakOptions::default())
+}
+
+fn org_to_html(in_str: &str) -> String {
+    // Converts the .org input string <in> to HTML.
+    let mut writer = Vec::new();
+    Org::parse(in_str).write_html(&mut writer).unwrap();
+    String::from_utf8(writer).unwrap()
+}
+
+// --------- ROUTING ---------
+
+#[get("/")]
+async fn index() -> HttpResponse {
+    let argv = Args::parse();
+    dispatch(&argv.defaultpage).await
+}
+
+#[get("/{file}")]
+async fn get_file(file: web::Path<String>) -> HttpResponse {
+    let filepath = file.into_inner();
+    dispatch(&filepath).await
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    // Sanity check: Do we even have a Zip archive?
+    // (This cannot be changed while the application is running,
+    //  so we won't have to check this later anymore.)
+    let args: Vec<_> = env::args().collect();
+    let zipfile = fs::File::open(Path::new(&args[0])).unwrap();
+    match ZipArchive::new(&zipfile) {
+        Err(_) => {
+            eprintln!("There is no Zip archive here. Exiting.");
+            process::exit(42);
+        }
+        Ok(_) => println!("Found a valid Zip archive."),
+    };
+
+    // Start and listen:
+    let argv = Args::parse();
+    println!("Starting zb on port {}.", argv.port);
+
+    HttpServer::new(|| App::new().service(index).service(get_file))
+        .bind(("0.0.0.0", argv.port))?
+        .run()
+        .await
+}
